5d815fb5c1989b126d547278e82f34ceed3329fa
Narzew <narzew@gmail.com>
2015-06-10 22:18:50 +0200

    Version 1.1

--- postrequester.rb
+++ postrequester.rb
@@ -1,7 +1,7 @@
 #=======================================================================
 #**Post Requester
-#**Copyright 09.06.2015 Narzew
-#**v 1.00
+#**Copyright 10.06.2015 Narzew
+#**v 1.1
 #=======================================================================
 
 require 'net/http'
@@ -9,12 +9,18 @@ require 'uri'
 
 module PostRequester
 
+	$logfile = "postrequester_log.txt"
+	$nolog = false
+	
 	def self.show_help
 		print "Usage: postrequester.rb URL field=value field=value !flag=value\n"
 		print "Available flags:\n"
-		print "sc - Split center - remove all before first and after second pattern\n"
-		print "sr - Split right - remove all before pattern\n"
-		print "sl - Split left - remove all after pattern\n"
+		print "sc=pat - Split center - remove all before first and after second pattern\n"
+		print "sr=pat - Split right - remove all before pattern\n"
+		print "sl=pat - Split left - remove all after pattern\n"
+		print "port=nr - Change port number\n"
+		print "nolog=1 - Do not log requests\n"
+		print "log=file - Change logfile to file\n"
 	end
 	
 	def self.print_error(x)
@@ -22,12 +28,23 @@ module PostRequester
 		self.show_help
 	end
 	
+	def self.to2digit(x)
+		if(x.to_i<10)
+			return "0#{x}"
+		else
+			return "#{x}"
+		end
+	end
+	
 	def self.return_response(x)
 		print "#{x}\n"
-		begin
-			File.open("postrequester_log.txt", "a"){|w| w.write(x+"\n") }
-		rescue
-			print "Warning: Failed to log response."
+		if $nolog == false
+			begin
+				resp = "#{self.to2digit(Time.now.day)}-#{self.to2digit(Time.now.month)}-#{self.to2digit(Time.now.year)} #{self.to2digit(Time.now.hour)}:#{self.to2digit(Time.now.min)}:#{self.to2digit(Time.now.sec)} #{ARGV.join(" ")} => #{x}\n"
+				File.open($logfile, "a"){|w| w.write(resp) }
+			rescue
+				print "Warning: Failed to log response."
+			end
 		end
 	end
 
@@ -51,15 +68,36 @@ module PostRequester
 					fields[data.at(0)] = data.at(1)
 				end
 			else
-				self.print_error("Invalid parameter: "+x)
-				PostRequester.show_help
+				if x.include?("!")
+					flags[x[0].gsub("!","")] = ""
+				else
+					self.print_error("Invalid parameter: "+x)
+					PostRequester.show_help
+				end
 			end
 			count += 1
 		}
 		
 		# Parse fields
 		uri = URI($url)
-		https = Net::HTTP.new(uri.host, uri.port)
+		if flags.include?("nolog")
+			if flags["nolog"] == "0" || flags["nolog"].downcase == "false"
+				$nolog = false
+			else
+				$nolog = true
+			end
+		end
+		if flags.include?("log")
+			$logfile = flags["log"]
+		end
+		if flags.include?("clear")
+			File.delete($logfile)
+		end
+		if flags.include?("port")
+			https = Net::HTTP.new(uri.host, flags["port"])
+		else
+			https = Net::HTTP.new(uri.host, uri.port)
+		end
 		response = https.post($url, URI.encode_www_form(fields))
 		result = response.body
 		if flags.include?("sc")
@@ -87,5 +125,5 @@ begin
 		PostRequester.show_help
 	end
 rescue => e
-	print "Error #{e}\n"
+	print "Error: #{e}\n"
 end
