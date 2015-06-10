88c45d5901803839a6d8e48f8f23547b64087a51
Narzew <narzew@gmail.com>
2015-06-10 23:18:37 +0200

    Version 1.3

--- postrequester.rb
+++ postrequester.rb
@@ -1,7 +1,7 @@
 #=======================================================================
 #**Post Requester
 #**Copyright 10.06.2015 Narzew
-#**v 1.2
+#**v 1.3
 #=======================================================================
 
 require 'net/http'
@@ -15,16 +15,27 @@ module PostRequester
 	$nolog = false
 	
 	def self.show_help
-		print "Usage: postrequester.rb URL field=value field=value !flag=value\n"
+		print "Usage: postrequester.rb URL field=value field=value @flag=value\n"
 		print "Available flags:\n"
-		print "sc=pat - Split center - remove all before first and after second pattern\n"
-		print "sr=pat - Split right - remove all before pattern\n"
-		print "sl=pat - Split left - remove all after pattern\n"
+		print "sc=pattern - Split center - remove all before first and after second pattern\n"
+		print "sr=pattern - Split right - remove all before pattern\n"
+		print "sl=pattern - Split left - remove all after pattern\n"
 		print "port=nr - Change port number\n"
 		print "nolog=1 - Do not log requests\n"
 		print "log=file - Change logfile to file\n"
 		print "sha1=field=value - Hash value with SHA1\n"
 		print "md5=field=value - Hash value with MD5\n"
+		print "b64=field=value - Encode value with Base64\n"
+		print "uue=field=value - Encode value with UUEncode\n"
+		print "db64=field=value - Decode value with Base64\n"
+		print "duue=field=value - Decode value with UUEncode\n"
+		print "file=field=value - Replace value with file data\n"
+		print "sha1file=field=value - Replace value with SHA1 file hash\n"
+		print "md5file=field=value - Replace value with MD5 file hash\n"
+		print "b64file=field=value - Replace value with Base64 encoded file data\n"
+		print "uuefile=field=value - Replace value with UUEncode encoded file data\n"
+		print "db64file=field=value - Replace value with Base64 decoded file data\n"
+		print "duuefile=field=value - Replace value with UUEncode decoded file data\n"
 	end
 	
 	def self.print_error(x)
@@ -64,31 +75,75 @@ module PostRequester
 			end
 			if x.include?("=")
 				data = x.split("=")
-				if data.at(0)[0] == "!"
+				if data.at(0)[0] == "@"
 					# Field is flag
 			
 					# Special flag treatmnet
-					if data.at(0) == "!sha1"
+					if data.at(0) == "@sha1"
 						fields[data.at(1)] = Digest::SHA1.hexdigest(data.at(2))
-					elsif data.at(0) == "!md5"
+					elsif data.at(0) == "@md5"
 						fields[data.at(1)] = Digest::MD5.hexdigest(data.at(2))
-					elsif data.at(0) == "!file"
+					elsif data.at(0) == "@b64"
+						fields[data.at(1)] = [data.at(2)].pack('m0').to_s
+					elsif data.at(0) == "@uue"
+						fields[data.at(1)] = [data.at(2)].pack('u').to_s
+					elsif data.at(0) == "@db64"
+						fields[data.at(1)] = data.at(2).unpack('m0').first.to_s
+					elsif data.at(0) == "@duue"
+						fields[data.at(1)] = data.at(2).unpack('u').first.to_s
+					elsif data.at(0) == "@file"
 						if File.exist?(data.at(2))
 							fields[data.at(1)] = File.read(data.at(2))
 						else
-							print_error("File #{data.at(2)} don't exist!")
+							print_error("File #{data.at(2)} don't exist@")
+						end
+					elsif data.at(0) == "@sha1file"
+						if File.exist?(data.at(2))
+							fields[data.at(1)] = Digest::SHA1.hexdigest(File.read(data.at(2)))
+						else
+							print_error("File #{data.at(2)} don't exist@")
+						end
+					elsif data.at(0) == "@md5file"
+						if File.exist?(data.at(2))
+							fields[data.at(1)] = Digest::MD5.hexdigest(File.read(data.at(2)))
+						else
+							print_error("File #{data.at(2)} don't exist@")
+						end
+					elsif data.at(0) == "@b64file"
+						if File.exist?(data.at(2))
+							fields[data.at(1)] = [File.read(data.at(2))].pack('m0').to_s
+						else
+							print_error("File #{data.at(2)} don't exist@")
+						end
+					elsif data.at(0) == "@uuefile"
+						if File.exist?(data.at(2))
+							fields[data.at(1)] = [File.read(data.at(2))].pack('u').to_s
+						else
+							print_error("File #{data.at(2)} don't exist@")
+						end
+					elsif data.at(0) == "@db64file"
+						if File.exist?(data.at(2))
+							fields[data.at(1)] = File.read(data.at(2)).unpack('m0').first
+						else
+							print_error("File #{data.at(2)} don't exist@")
+						end
+					elsif data.at(0) == "@duuefile"
+						if File.exist?(data.at(2))
+							fields[data.at(1)] = File.read(data.at(2)).unpack('u').first
+						else
+							print_error("File #{data.at(2)} don't exist@")
 						end
 					else
 						#Normal flag treatment
-						flags[data.at(0).gsub("!","")] = data.at(1)
+						flags[data.at(0).gsub("@","")] = data.at(1)
 					end
 				else
 					# Field is key=value field
 					fields[data.at(0)] = data.at(1)
 				end
 			else
-				if x.include?("!")
-					flags[x[0].gsub("!","")] = ""
+				if x.include?("@")
+					flags[x[0].gsub("@","")] = ""
 				else
 					self.print_error("Invalid parameter: "+x)
 					PostRequester.show_help
